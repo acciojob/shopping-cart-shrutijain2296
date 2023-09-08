812385d195c5852951996418d4a67d77895181da
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-09-08 11:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+const listItem = document.getElementById("listItem");
+const item =  document.getElementById("item-name-input");
+const price =  document.getElementById("item-price-input");
+const addItem = document.getElementById("addItem")
+const total = document.getElementById("total");
+
+let grandTotal = 0.0;
+
+addItem.addEventListener('click', () => {
+	const itemName = item.value.trim();
+	const itemPrice = parseFloat(price.value);
+
+	if(itemName !== "" && itemPrice >= 0){
+		
+		const newRow = listItem.querySelector('tbody').insertRow();
+		const itemCell = newRow.insertCell(0);
+		const priceCell = newRow.insertCell(1);
+
+		itemCell.textContent = itemName;
+		priceCell.textContent = `$${itemPrice.toFixed(2)}`;
+
+		grandTotal += itemPrice;
+
+		total.textContent = `Grand Total: $${grandTotal.toFixed(2)}`;
+
+		item.value = "";
+		price.value = "";
+	}
+	else{
+		alert("Please input all the fields");
+	}
+})
+
+
